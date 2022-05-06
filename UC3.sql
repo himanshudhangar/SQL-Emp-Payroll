c1bf2e2be7795b93b46a0a18529f537f1e59cca0
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-05-06 23:57:47 +0530

    Create Eployee Payroll data and service

---
+++ UC3.sql
@@ -0,0 +1,4 @@
+ use employee_payroll;
+ INSERT INTO employee_payroll( name, salary, start) VALUES('Bill', 1000000.00, '2018-01-03' ),('Terisa', 2000080.00, '2019-11-13'),('Charlie', 3000000.00, '2020-05-21' );
+ 
+ 
