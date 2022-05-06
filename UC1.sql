f039d4518781c7c0c2c209e1bcfba9751a68ef66
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-05-06 23:38:09 +0530

    Create Payroll service Database

---
+++ UC1.sql
@@ -0,0 +1,3 @@
+ use employee_payroll;
+create table employee_payroll(id    INT NOT NULL AUTO_INCREMENT,name VARCHAR(150) NOT NULL,salary Double NOT NULL,start DATE NOT NULL,PRIMARY KEY(id));
+ DESCRIBE employee_payroll;
