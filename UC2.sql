a0554e3b68266a01ec41d77146d8caa7aa887061
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-05-06 23:49:38 +0530

    Create Employee Payroll Table

---
+++ UC2.sql
@@ -0,0 +1,3 @@
+  create table employee_payroll(id    INT NOT NULL AUTO_INCREMENT,name VARCHAR(150) NOT NULL,salary Double NOT NULL,start DATE NOT NULL,PRIMARY KEY(id));
+    use employee_payroll;
+    DESCRIBE employee_payroll;
