716f90c4b65113716e9a7b29a977c0e9545e3727
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-05-07 14:53:49 +0530

    Ability To Make Terissa as Part of Sales Marketing Department

---
+++ UC10.sql
@@ -0,0 +1,3 @@
+use  employee_payroll;
+insert into employee_payroll (id,name,phone_number,address,department,gender,basic_pay,deductions,taxable_pay,tax,net_pay,start) values(5,'Terissa','9888888888','Pune','Sales','F',4900000,100000,50000,5400,4000000,'2021-05-06');
+insert into Employee_payroll (id,name,phone_number,address,department,gender,basic_pay,deductions,taxable_pay,tax,net_pay,start) values(6,'Terissa','9888888888','Banglore','Marketing','F',4800000,100000,40000,5400,4000000,'2021-05-06');
